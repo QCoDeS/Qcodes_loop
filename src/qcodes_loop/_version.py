c449096f0987eef4ac1e6715eaf63769c3b6adb2
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2023-01-30 21:05:20 +0100

    fix versioningit logic

--- src/qcodes_loop/_version.py
+++ src/qcodes_loop/_version.py
@@ -6,7 +6,7 @@ def _get_version() -> str:
     import qcodes_loop
 
     module_path = Path(qcodes_loop.__file__).parent
-    return versioningit.get_version(project_dir=module_path.parent)
+    return versioningit.get_version(project_dir=module_path.parent.parent)
 
 
 __version__ = _get_version()
